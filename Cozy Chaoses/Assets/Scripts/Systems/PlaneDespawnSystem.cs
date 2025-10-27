07a3606561358d9e4d7abc944500593019ddc110
albe2669 <albert@risenielsen.dk>
2025-10-27 23:04:21 +0100

    Add despawn system and rewrite movement to jobs

---
+++ Cozy Chaoses/Assets/Scripts/Systems/PlaneDespawnSystem.cs
@@ -0,0 +1,40 @@
+using Unity.Entities;
+using Unity.Burst;
+using Unity.Mathematics;
+using Unity.Rendering;
+using Unity.Transforms;
+using Unity.VisualScripting;
+using Random = Unity.Mathematics.Random;
+
+public partial struct PlaneDespawnSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+        state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        state.RequireForUpdate<ConfigComponent>();
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
+            .CreateCommandBuffer(state.WorldUnmanaged);
+        
+        state.Dependency = new DespawnPlanes
+        {
+            ECB = ecb,
+        }.Schedule(state.Dependency);
+    }
+}
+
+[BurstCompile]
+[WithAll(typeof(ShouldDespawnComponent))]
+public partial struct DespawnPlanes : IJobEntity
+{
+    public EntityCommandBuffer ECB;
+    public void Execute(Entity entity)
+    {
+        ECB.DestroyEntity(entity);
+    }
+}
--- Cozy Chaoses/Assets/Scripts/Systems/PlaneMovementSystem.cs
+++ Cozy Chaoses/Assets/Scripts/Systems/PlaneMovementSystem.cs
@@ -10,66 +10,84 @@ public partial struct PlaneMovementSystem : ISystem
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
+        state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
+        state.RequireForUpdate<PlanetComponent>();
         state.RequireForUpdate<ConfigComponent>();
     }
     
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
-        var config = SystemAPI.GetSingleton<ConfigComponent>();
-        float sphereRadius = config.PlanetRadius;
-        float3 sphereCenter = new float3(0, 0, 0);
+        var ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
+            .CreateCommandBuffer(state.WorldUnmanaged);
+        var planet = SystemAPI.GetSingleton<PlanetComponent>();
+        var deltaTime = (float)SystemAPI.Time.DeltaTime;
+        
+        state.Dependency = new MovePlanes
+        {
+            ECB = ecb,
+            DeltaTime = deltaTime,
+            Planet = planet
+        }.Schedule(state.Dependency);
+    }
+}
+
+[BurstCompile]
+[WithAll(typeof(PlaneComponent))]
+[WithNone(typeof(ShouldDespawnComponent))]
+public partial struct MovePlanes : IJobEntity
+{
+    public EntityCommandBuffer ECB;
+    public float DeltaTime;
+    public PlanetComponent Planet;
+    
+    public void Execute(Entity entity, ref LocalTransform transform, ref URPMaterialPropertyBaseColor color, in PlaneComponent plane)
+    {
+        float sphereRadius = Planet.Radius;
+        float3 sphereCenter = Planet.Center;
         float speed = 5.0f;
         
-        var dt = SystemAPI.Time.DeltaTime;
+        // Tanks tutorial note: Modify the point at which we sample the 3D noise function.
+        var yPos = transform.Position;
+        yPos.y = (float)entity.Index;
 
-        foreach (var (transform, color, entity) in
-                 SystemAPI.Query<RefRW<LocalTransform>, RefRW<URPMaterialPropertyBaseColor>>()
-                     .WithAll<PlaneComponent>()
-                     .WithEntityAccess())
+        float3 dest = plane.Dest;
+        float3 pos = transform.Position;
+        
+        // Placeholder for despawn behavior
+        if (HasArrived(pos, dest, sphereCenter))
         {
-            // Tanks tutorial note: Modify the point at which we sample the 3D noise function.
-            var yPos = transform.ValueRO.Position;
-            yPos.y = (float)entity.Index;
-            
-            float3 dest = state.GetComponentLookup<PlaneComponent>()[entity].Dest;
-            float3 pos = transform.ValueRO.Position;
-            
-            // Placeholder for despawn behavior
-            ColorIfArrived(pos, dest, sphereCenter, color);
-            
-            float3 toCenter = math.normalize(pos - sphereCenter);
-            float3 toDest = math.normalize(dest - pos); 
-            
-            // NEW POSITION
-            float3 surfaceTangent = math.normalize(toDest - toCenter * math.dot(toDest, toCenter));
-            
-            float3 rotationAxis = math.normalize(math.cross(toCenter, surfaceTangent));
-            float rotationAngle = dt * speed / (sphereRadius + 5f);
-
-            quaternion rot = quaternion.AxisAngle(rotationAxis, rotationAngle);
-            float3 newDirection = math.mul(rot, toCenter);
-            float3 newSurfacePos = sphereCenter + newDirection * (sphereRadius + 5.0f);
-
-            transform.ValueRW.Position = newSurfacePos;
-
-            // NEW ROTATION
-            float3 forward = math.normalize(newSurfacePos - pos);
-            float3 up = math.normalize(pos - sphereCenter);
-            quaternion newRotation = quaternion.LookRotation(forward, up);
-            transform.ValueRW.Rotation = newRotation;
+            ECB.AddComponent(entity, new ShouldDespawnComponent());
+            return;
         }
-    }
+        
+        float3 toCenter = math.normalize(pos - sphereCenter);
+        float3 toDest = math.normalize(dest - pos); 
+        
+        // NEW POSITION
+        float3 surfaceTangent = math.normalize(toDest - toCenter * math.dot(toDest, toCenter));
+        
+        float3 rotationAxis = math.normalize(math.cross(toCenter, surfaceTangent));
+        float rotationAngle = DeltaTime * speed / (sphereRadius + 5f);
 
-    private void ColorIfArrived(float3 pos, float3 dest, float3 sphereCenter, RefRW<URPMaterialPropertyBaseColor> color)
+        quaternion rot = quaternion.AxisAngle(rotationAxis, rotationAngle);
+        float3 newDirection = math.mul(rot, toCenter);
+        float3 newSurfacePos = sphereCenter + newDirection * (sphereRadius + 5.0f);
+
+        transform.Position = newSurfacePos;
+
+        // NEW ROTATION
+        float3 forward = math.normalize(newSurfacePos - pos);
+        float3 up = math.normalize(pos - sphereCenter);
+        quaternion newRotation = quaternion.LookRotation(forward, up);
+        transform.Rotation = newRotation;
+    }
+    
+    private bool HasArrived(float3 pos, float3 dest, float3 sphereCenter)
     {
         float3 currentDir = math.normalize(pos - sphereCenter); 
         float3 destDir = math.normalize(dest - sphereCenter);
-            
-        if (math.dot(currentDir, destDir) > 0.999f) 
-        {
-            // despawn here
-            color.ValueRW.Value = new float4(0, 0, 0, 1); 
-        }
+
+        return math.dot(currentDir, destDir) > 0.999f;
     }
 }
