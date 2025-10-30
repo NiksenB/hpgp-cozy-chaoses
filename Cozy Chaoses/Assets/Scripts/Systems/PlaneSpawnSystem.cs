bf8b1b1b4641ec2786cff4147e3e9fd3849f956a
Nikoline Burman <nibu@itu.dk>
2025-10-30 13:14:15 +0100

    Use ECB for plane spawn

--- Cozy Chaoses/Assets/Scripts/Systems/PlaneSpawnSystem.cs
+++ Cozy Chaoses/Assets/Scripts/Systems/PlaneSpawnSystem.cs
@@ -4,6 +4,8 @@ using Unity.Collections;
 using Unity.Mathematics;
 using Unity.Rendering;
 using Unity.Transforms;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph;
 using Random = Unity.Mathematics.Random;
 
 [UpdateBefore(typeof(TransformSystemGroup))]
@@ -11,14 +13,16 @@ public partial struct PlaneSpawnSystem : ISystem
 {
     private float timer;
     private Random random;
-    
+
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<ConfigComponent>();
+        state.RequireForUpdate<PlanetComponent>();
+        state.RequireForUpdate<AirportComponent>();
         random = new Random(72);
     }
-    
+
     [BurstCompile]
     public void OnUpdate(ref SystemState state)
     {
@@ -26,44 +30,60 @@ public partial struct PlaneSpawnSystem : ISystem
         timer -= SystemAPI.Time.DeltaTime;
         if (timer > 0)
         {
-            return; 
-        } 
-        timer = 1.5f;   // reset timer
-
-        var config = SystemAPI.GetSingleton<ConfigComponent>();
-        var radius = config.PlanetRadius;
-        
-        var planeTransform = state.EntityManager.GetComponentData<LocalTransform>(config.PlanePrefab);
-        
-        foreach (var (airport, airportTransform, color) in
-                 SystemAPI.Query<RefRO<AirportComponent>, RefRO<LocalToWorld>, RefRO<URPMaterialPropertyBaseColor>>())
-        {
-            Entity planeEntity = state.EntityManager.Instantiate(config.PlanePrefab);
-            
-            var linkedEntities = state.EntityManager.GetBuffer<LinkedEntityGroup>(planeEntity);
-            foreach (var entity in linkedEntities)
-            {
-                if (state.EntityManager.HasComponent<URPMaterialPropertyBaseColor>(entity.Value))
-                {
-                    state.EntityManager.SetComponentData(entity.Value, color.ValueRO);    
-                }
-            }
-            
-            // PLACEHOLDER DESTINATION
-            float3 r = new float3(
-                random.NextFloat(-100f, 100f),
-                random.NextFloat(-100f, 100f),
-                random.NextFloat(-100f, 100f)
-            );
-            float3 dest = float3.zero + math.normalize(r - float3.zero) * (radius + 5f);
-            
-            planeTransform.Position =  airportTransform.ValueRO.Position;
-            
-            state.EntityManager.SetComponentData(planeEntity, planeTransform);
-            state.EntityManager.SetComponentData(planeEntity, new PlaneComponent
-            {
-                Dest = dest
-            });
+            return;
         }
+
+        timer = 1.5f; // reset timer
+        
+        var ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
+            .CreateCommandBuffer(state.WorldUnmanaged);
+        // Read the spawned planet from the world and pass it into the job
+        var planet = SystemAPI.GetSingleton<PlanetComponent>();
+        var config = SystemAPI.GetSingleton<ConfigComponent>();
+        
+        state.Dependency = new SpawnPlanes
+        {
+            ECB = ecb,
+            Planet = planet,
+            Config = config,
+            Random =  random
+            
+        }.Schedule(state.Dependency);
+
+    }
+}
+
+[BurstCompile]
+[WithAll(typeof(AirportComponent))]
+public partial struct SpawnPlanes : IJobEntity
+{
+    public EntityCommandBuffer ECB;
+    public PlanetComponent Planet;
+    public ConfigComponent Config;
+    public Random Random;
+    private void Execute(ref LocalToWorld airport, ref URPMaterialPropertyBaseColor color)
+    {
+        var sphereCenter = Planet.Center;
+        var sphereRadius = Planet.Radius;
+        
+        Entity planeEntity = ECB.Instantiate(Config.PlanePrefab);
+        
+        // PLACEHOLDER DESTINATION
+        float3 r = new float3(
+            Random.NextFloat(-100f, 100f),
+            Random.NextFloat(-100f, 100f),
+            Random.NextFloat(-100f, 100f)
+        );
+        float3 dest = sphereCenter + math.normalize(r - sphereCenter) * (sphereRadius + 5f);
+
+        var planeTransform = LocalTransform.FromPosition(airport.Position);
+        
+        ECB.AddComponent(planeEntity, planeTransform);
+        ECB.SetComponent(planeEntity, color);
+        ECB.AddComponent(planeEntity, new PlaneComponent
+        {
+            Dest = dest
+        });
+        
     }
 }
