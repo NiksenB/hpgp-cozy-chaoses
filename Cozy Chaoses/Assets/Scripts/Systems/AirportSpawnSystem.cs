f0a5db6ba94ce2dfbbee618644064a13210880fe
Nikoline Burman <nibu@itu.dk>
2025-11-04 17:28:03 +0100

    Spawn equidistant airports on planet
    - Avoids overlap no matter amount of plannets. Jitter can later be used to simulate organic/random placements

--- Cozy Chaoses/Assets/Scripts/Systems/AirportSpawnSystem.cs
+++ Cozy Chaoses/Assets/Scripts/Systems/AirportSpawnSystem.cs
@@ -3,6 +3,8 @@ using Unity.Burst;
 using Unity.Mathematics;
 using Unity.Rendering;
 using Unity.Transforms;
+using Unity.Collections;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using Random = Unity.Mathematics.Random;
 
@@ -13,7 +15,6 @@ public partial struct AirportSpawnSystem : ISystem
     {
         state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
         state.RequireForUpdate<ConfigComponent>();
-        // Ensure a planet exists before we try to spawn airports that depend on it
         state.RequireForUpdate<PlanetComponent>();
     }
 
@@ -25,14 +26,18 @@ public partial struct AirportSpawnSystem : ISystem
         
         var ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
             .CreateCommandBuffer(state.WorldUnmanaged);
-        // Read the spawned planet from the world and pass it into the job
+
         var planet = SystemAPI.GetSingleton<PlanetComponent>();
+        var positions = new NativeArray<float3>(SystemAPI.GetSingleton<ConfigComponent>().AirportCount, Allocator.TempJob);
         
         state.Dependency = new SpawnAirports
         {
             ECB = ecb,
-            Planet = planet
+            Planet = planet,
+            Positions = positions
         }.Schedule(state.Dependency);
+        state.Dependency.Complete();
+        positions.Dispose();
     }
 }
 
@@ -41,12 +46,14 @@ public partial struct SpawnAirports : IJobEntity
 {
     public EntityCommandBuffer ECB;
     public PlanetComponent Planet;
-    public void Execute(in ConfigComponent configComponent)
+    public NativeArray<float3> Positions;
+    private void Execute(in ConfigComponent configComponent)
     {
         var sphereCenter = Planet.Center;
         var sphereRadius = Planet.Radius;
 
         var random = new Random(72);
+        var positions = GeneratePoints(configComponent.AirportCount, Positions, sphereRadius);
         
         for (var i = 0; i < configComponent.AirportCount; i++)
         {
@@ -60,13 +67,7 @@ public partial struct SpawnAirports : IJobEntity
             
             ECB.SetComponent(airportEntity, new AirportComponent { NextPlaneSpawnTime = random.NextDouble(2d, 10d) });
             
-            var newPos = new float3(
-                random.NextFloat(-100f, 100f),
-                random.NextFloat(-100f, 100f),
-                random.NextFloat(-100f, 100f)
-            );
-
-            var up = math.normalize(newPos - sphereCenter);
+            var up = math.normalize(positions[i] - sphereCenter);
             var rot = Quaternion.FromToRotation(Vector3.up, up);
             var pos = sphereCenter + up * sphereRadius;
             var scale = sphereRadius * 0.05f; // Relative to planet size
@@ -75,5 +76,26 @@ public partial struct SpawnAirports : IJobEntity
             
             ECB.AddComponent(airportEntity, transform);
         }
+    }
+    
+    // Below is from tutorial: https://devforum.roblox.com/t/generating-equidistant-points-on-a-sphere/874144
+    private static NativeArray<float3> GeneratePoints(int n, NativeArray<float3> arr, float sphereRadius)
+    {
+        var goldenRatio = (1 + math.sqrt(5)) / 2;
+        var angleIncrement = 2 * math.PI * goldenRatio;
+    
+        for (var i = 0; i < n; i++)
+        {
+            var distance = (float)i / n;
+            var incline = math.acos(1 - 2 * distance);
+            var azimuth = angleIncrement * i;
+    
+            var x = math.sin(incline) * math.cos(azimuth) * sphereRadius;
+            var y = math.sin(incline) * math.sin(azimuth) * sphereRadius;
+            var z = math.cos(incline) * sphereRadius;
+        
+            arr[i] = new float3(x, y, z);
+        }
+        return arr;
     }
 }
