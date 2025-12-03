d4336f5039fd3b90c20c94fbb6882442728e9e85
Nikoline Burman <nibu@itu.dk>
2025-12-03 23:55:14 +0100

    Simplify NavigationCalculator via switch statement

--- Cozy Chaoses/Assets/Scripts/NavigationCalculator.cs
+++ Cozy Chaoses/Assets/Scripts/NavigationCalculator.cs
@@ -7,10 +7,12 @@ using Unity.Transforms;
 
     public class NavigationCalculator
     {
-        private static readonly float planeSpeed = 4f;
+        private enum Dir { Up, Down, Forward, End }
+
+        private static readonly float Speed = 5f;
         
-        private float flyDegLimit = 50f;  
-        private float frameChangeDegLimit = 0.5f;
+        private static readonly float VerticalDegLimit = 50f;  
+        private static readonly float FrameChangeDegLimit = 0.5f;
         public static (float3, quaternion) CalculateNext(LocalTransform  transform, GuidePathComponent guidePath, float planetRadius, float deltaTime)
         {
             float height = math.length(transform.Position - new float3(planetRadius, planetRadius, planetRadius));
@@ -22,21 +24,22 @@ using Unity.Transforms;
                     
                 if (math.abs(hDiff) <= 5f)
                 {
-                    return GoForward(transform, guidePath, planetRadius, deltaTime);
+                    return Calculate(transform, guidePath, planetRadius, deltaTime, Dir.Forward);
                 }
                 
                 if (hDiff > 5f)
                 {
-                    return GoUpwards(transform, guidePath, planetRadius, deltaTime);
+                    return Calculate(transform, guidePath, planetRadius, deltaTime, Dir.Up);
                 }
 
-                return GoDownwards(transform, guidePath, planetRadius, deltaTime);
+                return Calculate(transform, guidePath, planetRadius, deltaTime, Dir.Down);
             }
             
-            return GoLand(transform, guidePath, planetRadius, deltaTime);
+            return Calculate(transform, guidePath, planetRadius, deltaTime, Dir.End);
         }
 
-        private static (float3, quaternion) GoForward(LocalTransform transform, GuidePathComponent guidePath, float planetRadius, float deltaTime)
+        private static (float3, quaternion) Calculate(LocalTransform transform, GuidePathComponent guidePath,
+            float planetRadius, float deltaTime, Dir dir)
         {
             float3 up = math.normalize(transform.Position);
             float3 toDest = guidePath.EndPoint - transform.Position;
@@ -45,10 +48,10 @@ using Unity.Transforms;
 
             if (dot < 0.99f)
             {
-                // adjust course so we only rotate 2 degrees
+                // limit horizontal rotation pr frame
                 float3 cross = math.cross(currentForward, toDest);
                 float sign = math.sign(math.dot(cross, up));
-                quaternion smallTurn = quaternion.AxisAngle(up, math.radians(2f) * sign);
+                quaternion smallTurn = quaternion.AxisAngle(up, math.radians(FrameChangeDegLimit) * sign);
                 toDest = math.mul(smallTurn, currentForward);
             }
 
@@ -56,111 +59,20 @@ using Unity.Transforms;
             float3 surfaceTangent = math.normalize(toDest - up * math.dot(toDest, up));
 
             float3 rotationAxis = math.normalize(math.cross(up, surfaceTangent));
-            float rotationAngle = deltaTime * planeSpeed / (planetRadius + guidePath.TargetHeight);
+            float rotationAngle = deltaTime * Speed / (planetRadius + guidePath.TargetHeight);
 
             quaternion rot = quaternion.AxisAngle(rotationAxis, rotationAngle);
             float3 newDirection = math.mul(rot, up);
-            float3 newSurfacePos = newDirection * (planetRadius + guidePath.TargetHeight);
 
-            // NEW ROTATION
-            float3 forward = math.normalize(newSurfacePos - transform.Position);
-            quaternion newRotation = quaternion.LookRotation(forward, up);
-
-            return (newSurfacePos, newRotation);
-        }
-        
-        private static (float3, quaternion) GoUpwards(LocalTransform transform, GuidePathComponent guidePath, float planetRadius, float deltaTime)
-        {
-            float3 up = math.normalize(transform.Position);
-            float3 toDest = guidePath.EndPoint - transform.Position;
-            float3 currentForward = math.normalize(math.forward(transform.Rotation.value));
-            var dot = math.dot(toDest, currentForward);
-
-            if (dot < 0.99f)
+            float3 newSurfacePos = dir switch
             {
-                // adjust course so we only rotate 2 degrees
-                float3 cross = math.cross(currentForward, toDest);
-                float sign = math.sign(math.dot(cross, up));
-                quaternion smallTurn = quaternion.AxisAngle(up, math.radians(2f) * sign);
-                toDest = math.mul(smallTurn, currentForward);
-            }
-
-            // NEW POSITION
-            float3 surfaceTangent = math.normalize(toDest - up * math.dot(toDest, up));
-
-            float3 rotationAxis = math.normalize(math.cross(up, surfaceTangent));
-            float rotationAngle = deltaTime * planeSpeed / (planetRadius + guidePath.TargetHeight);
-
-            quaternion rot = quaternion.AxisAngle(rotationAxis, rotationAngle);
-            float3 newDirection = math.mul(rot, up);
-            float3 newSurfacePos = newDirection * math.min(planetRadius + guidePath.TargetHeight, math.length(transform.Position)*1.0001f);
-
-            // NEW ROTATION
-            float3 forward = math.normalize(newSurfacePos - transform.Position);
-            quaternion newRotation = quaternion.LookRotation(forward, up);
-
-            return (newSurfacePos, newRotation);
-        }
-        
-        private static (float3, quaternion) GoDownwards(LocalTransform transform, GuidePathComponent guidePath, float planetRadius, float deltaTime)
-        {
-            float3 up = math.normalize(transform.Position);
-            float3 toDest = guidePath.EndPoint - transform.Position;
-            float3 currentForward = math.normalize(math.forward(transform.Rotation.value));
-            var dot = math.dot(toDest, currentForward);
-
-            if (dot < 0.99f)
-            {
-                // adjust course so we only rotate 2 degrees
-                float3 cross = math.cross(currentForward, toDest);
-                float sign = math.sign(math.dot(cross, up));
-                quaternion smallTurn = quaternion.AxisAngle(up, math.radians(2f) * sign);
-                toDest = math.mul(smallTurn, currentForward);
-            }
-
-            // NEW POSITION
-            float3 surfaceTangent = math.normalize(toDest - up * math.dot(toDest, up));
-
-            float3 rotationAxis = math.normalize(math.cross(up, surfaceTangent));
-            float rotationAngle = deltaTime * planeSpeed / (planetRadius + guidePath.TargetHeight);
-
-            quaternion rot = quaternion.AxisAngle(rotationAxis, rotationAngle);
-            float3 newDirection = math.mul(rot, up);
-            float3 newSurfacePos = newDirection * math.max(planetRadius+1f, math.length(transform.Position)*0.9999f);
-
-            // NEW ROTATION
-            float3 forward = math.normalize(newSurfacePos - transform.Position);
-            quaternion newRotation = quaternion.LookRotation(forward, up);
-
-            return (newSurfacePos, newRotation);
-        }
-
-        private static (float3, quaternion) GoLand(LocalTransform transform, GuidePathComponent guidePath, float planetRadius, float deltaTime)
-        {
-            float3 up = math.normalize(transform.Position);
-            float3 toDest = guidePath.EndPoint - transform.Position;
-            float3 currentForward = math.normalize(math.forward(transform.Rotation.value));
-            var dot = math.dot(toDest, currentForward);
-
-            if (dot < 0.99f)
-            {
-                // adjust course so we only rotate 2 degrees
-                float3 cross = math.cross(currentForward, toDest);
-                float sign = math.sign(math.dot(cross, up));
-                quaternion smallTurn = quaternion.AxisAngle(up, math.radians(2f) * sign);
-                toDest = math.mul(smallTurn, currentForward);
-            }
-
-            // NEW POSITION
-            float3 surfaceTangent = math.normalize(toDest - up * math.dot(toDest, up));
-
-            float3 rotationAxis = math.normalize(math.cross(up, surfaceTangent));
-            float rotationAngle = deltaTime * planeSpeed / (planetRadius + guidePath.TargetHeight);
-
-            quaternion rot = quaternion.AxisAngle(rotationAxis, rotationAngle);
-            float3 newDirection = math.mul(rot, up);
-            float3 newSurfacePos = newDirection * math.max(planetRadius+1f, math.length(transform.Position)*0.99f);
-
+                Dir.Forward => newDirection * (planetRadius + guidePath.TargetHeight),
+                Dir.Up => newDirection * math.min(planetRadius + guidePath.TargetHeight, math.length(transform.Position)*1.0001f),
+                Dir.Down => newDirection * math.max(planetRadius+1f, math.length(transform.Position)*0.9999f),
+                Dir.End => newDirection * math.max(planetRadius, math.length(transform.Position)*0.9999f),
+                _ => throw new ArgumentOutOfRangeException()
+            };
+            
             // NEW ROTATION
             float3 forward = math.normalize(newSurfacePos - transform.Position);
             quaternion newRotation = quaternion.LookRotation(forward, up);
