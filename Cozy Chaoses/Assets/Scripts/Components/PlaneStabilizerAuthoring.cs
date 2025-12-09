b55010b2022e32d4babadb15046831201b4ebd66
albe2669 <albert@risenielsen.dk>
2025-12-09 16:16:36 +0100

    Remove extra stuff from stabilizer that has been moved to config

--- Cozy Chaoses/Assets/Scripts/Components/PlaneStabilizerAuthoring.cs
+++ Cozy Chaoses/Assets/Scripts/Components/PlaneStabilizerAuthoring.cs
@@ -4,12 +4,6 @@ using UnityEngine;
 public class PlaneStabilizerAuthoring : MonoBehaviour
 {
     public GameObject guideObject; // Reference to the guide entity
-    public float rotationSpeed = 6f; // How fast to rotate towards target
-    public float damping = 7f; // Damping factor for angular velocity
-    public float maxAngularSpeed = 6f; // Limit on angular velocity
-    public float responseSpeed = 8f; // How quickly to correct orientation
-    public float forwardWeight = 1.0f; // Weight for forward alignment
-    public float upWeight = 0.5f; // Weight for up alignment
 
     private class Baker : Baker<PlaneStabilizerAuthoring>
     {
@@ -21,12 +15,6 @@ public class PlaneStabilizerAuthoring : MonoBehaviour
             AddComponent(entity, new PlaneStabilizerComponent
             {
                 GuideEntity = guideEntity,
-                RotationSpeed = authoring.rotationSpeed,
-                Damping = authoring.damping,
-                MaxAngularSpeed = authoring.maxAngularSpeed,
-                ResponseSpeed = authoring.responseSpeed,
-                ForwardWeight = authoring.forwardWeight,
-                UpWeight = authoring.upWeight
             });
         }
     }
@@ -35,10 +23,4 @@ public class PlaneStabilizerAuthoring : MonoBehaviour
 public struct PlaneStabilizerComponent : IComponentData
 {
     public Entity GuideEntity;
-    public float RotationSpeed;
-    public float Damping;
-    public float MaxAngularSpeed;
-    public float ResponseSpeed;
-    public float ForwardWeight;
-    public float UpWeight;
 }
