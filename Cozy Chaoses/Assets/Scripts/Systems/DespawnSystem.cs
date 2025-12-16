01ec1cba97888c32aa58ba0a7327563ef865f570
albe2669 <albert@risenielsen.dk>
2025-12-16 12:13:56 +0100

    Fix the counter using native reference and some unsafe code

--- Cozy Chaoses/Assets/Scripts/Systems/DespawnSystem.cs
+++ Cozy Chaoses/Assets/Scripts/Systems/DespawnSystem.cs
@@ -1,22 +1,32 @@
+using System.Threading;
 using Unity.Burst;
+using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 
 public partial struct DespawnSystem : ISystem
 {
+    private NativeReference<int> _planesDespawned;
+    
     [BurstCompile]
     public void OnCreate(ref SystemState state)
     {
         state.RequireForUpdate<BeginSimulationEntityCommandBufferSystem.Singleton>();
         state.RequireForUpdate<ConfigComponent>();
+        _planesDespawned = new NativeReference<int>(Allocator.Persistent);
     }
 
     [BurstCompile]
-    public void OnUpdate(ref SystemState state)
+    public unsafe void OnUpdate(ref SystemState state)
     {
         var ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
             .CreateCommandBuffer(state.WorldUnmanaged);
 
-        var config = SystemAPI.GetSingleton<ConfigComponent>();
+        var configRW = SystemAPI.GetSingletonRW<ConfigComponent>();
+        var config = configRW.ValueRO;
+
+        _planesDespawned.Value = 0;
+        var counterRef = _planesDespawned.GetUnsafePtr();
 
         switch (config.ExecutionMode)
         {
@@ -24,23 +34,36 @@ public partial struct DespawnSystem : ISystem
                 foreach (var (_, entity) in SystemAPI.Query<RefRO<ShouldDespawnTag>>().WithEntityAccess())
                 {
                     ecb.DestroyEntity(entity);
+                    Interlocked.Increment(ref UnsafeUtility.AsRef<int>(counterRef));
                 }
                 break;
 
             case ExecutionMode.Schedule:
                 state.Dependency = new DespawnJobSingle
                 {
-                    ECB = ecb
+                    ECB = ecb,
+                    CounterRef = counterRef,
                 }.Schedule(state.Dependency);
                 break;
 
             case ExecutionMode.ScheduleParallel:
                 state.Dependency = new DespawnJobParallel
                 {
-                    ECB = ecb.AsParallelWriter()
+                    ECB = ecb.AsParallelWriter(),
+                    CounterRef = counterRef,
                 }.ScheduleParallel(state.Dependency);
                 break;
         }
+        
+        state.Dependency.Complete();
+        configRW.ValueRW.CurrentPlaneCount -= _planesDespawned.Value;
+    }
+
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+        if (_planesDespawned.IsCreated)
+            _planesDespawned.Dispose();
     }
 }
 
@@ -49,10 +72,12 @@ public partial struct DespawnSystem : ISystem
 public partial struct DespawnJobSingle : IJobEntity
 {
     public EntityCommandBuffer ECB;
+    [NativeDisableUnsafePtrRestriction] public unsafe int* CounterRef;
 
-    public void Execute(Entity entity)
+    public unsafe void Execute(Entity entity)
     {
         ECB.DestroyEntity(entity);
+        Interlocked.Increment(ref UnsafeUtility.AsRef<int>(CounterRef));
     }
 }
 
@@ -61,9 +86,11 @@ public partial struct DespawnJobSingle : IJobEntity
 public partial struct DespawnJobParallel : IJobEntity
 {
     public EntityCommandBuffer.ParallelWriter ECB;
+    [NativeDisableUnsafePtrRestriction] public unsafe int* CounterRef;
 
-    public void Execute([ChunkIndexInQuery] int chunkIndex, Entity entity)
+    public unsafe void Execute([ChunkIndexInQuery] int chunkIndex, Entity entity)
     {
         ECB.DestroyEntity(chunkIndex, entity);
+        Interlocked.Increment(ref UnsafeUtility.AsRef<int>(CounterRef));
     }
 }
--- Cozy Chaoses/Assets/Scripts/Systems/PlaneAndGuideSpawnSystem.cs
+++ Cozy Chaoses/Assets/Scripts/Systems/PlaneAndGuideSpawnSystem.cs
@@ -1,5 +1,7 @@
+using System.Threading;
 using Unity.Burst;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Entities;
 using Unity.Mathematics;
 using Unity.Transforms;
@@ -22,7 +24,7 @@ public partial struct PlaneAndGuideSpawnSystem : ISystem
     }
 
     [BurstCompile]
-    public void OnUpdate(ref SystemState state)
+    public unsafe void OnUpdate(ref SystemState state)
     {
         var ecb = SystemAPI.GetSingleton<BeginSimulationEntityCommandBufferSystem.Singleton>()
             .CreateCommandBuffer(state.WorldUnmanaged);
@@ -39,24 +41,29 @@ public partial struct PlaneAndGuideSpawnSystem : ISystem
         }
 
         var elapsedTime = SystemAPI.Time.ElapsedTime;
+        _planesSpawned.Value = 0;
+        var counterRef = _planesSpawned.GetUnsafePtr();
 
         switch (config.ExecutionMode)
         {
             case ExecutionMode.Main:
-                foreach (var (airportComponent, sourceTransform) in SystemAPI.Query<RefRW<AirportComponent>, RefRO<LocalTransform>>()
+                foreach (var (airportComponent, sourceTransform) in SystemAPI
+                             .Query<RefRW<AirportComponent>, RefRO<LocalTransform>>()
                              .WithAll<AirportComponent, LocalTransform>())
                 {
                     if (elapsedTime < airportComponent.ValueRO.NextPlaneSpawnTime) continue;
-                    if (configRW.ValueRO.CurrentPlaneCount >= config.MaxPlaneCount) continue;
-                    configRW.ValueRW.CurrentPlaneCount++;
+                    if (configRW.ValueRO.CurrentPlaneCount + *counterRef >= config.MaxPlaneCount) continue;
+                    Interlocked.Increment(ref UnsafeUtility.AsRef<int>(counterRef));
 
                     var random = new Random((uint)elapsedTime + 100);
-                    airportComponent.ValueRW.NextPlaneSpawnTime += random.NextDouble(config.NextPlaneSpawnTimeLower, config.NextPlaneSpawnTimeUpper);
+                    airportComponent.ValueRW.NextPlaneSpawnTime += random.NextDouble(config.NextPlaneSpawnTimeLower,
+                        config.NextPlaneSpawnTimeUpper);
                     var planeAndGuideEntity = ecb.Instantiate(config.PlanePrefab);
 
                     var di = math.abs(random.NextInt()) % _airports.Length;
 
-                    while (sourceTransform.ValueRO.Position.Equals(_airports[di].Position)) di = (di + 1) % _airports.Length;
+                    while (sourceTransform.ValueRO.Position.Equals(_airports[di].Position))
+                        di = (di + 1) % _airports.Length;
 
                     var dest = _airports[di].Position;
                     var dist = math.length(dest - sourceTransform.ValueRO.Position);
@@ -87,6 +94,7 @@ public partial struct PlaneAndGuideSpawnSystem : ISystem
                             0.05f * config.PlanetRadius * (dist / config.PlanetRadius))
                     });
                 }
+
                 break;
 
             case ExecutionMode.Schedule:
@@ -97,26 +105,25 @@ public partial struct PlaneAndGuideSpawnSystem : ISystem
                     Config = config,
                     ElapsedTime = elapsedTime,
                     Airports = _airports,
-                    PlanesSpawned = _planesSpawned
+                    CounterRef = counterRef
                 }.Schedule(state.Dependency);
                 state.Dependency.Complete();
-                configRW.ValueRW.CurrentPlaneCount += _planesSpawned.Value;
                 break;
 
             case ExecutionMode.ScheduleParallel:
-                _planesSpawned.Value = 0;
                 state.Dependency = new SpawnPlanesParallel
                 {
                     ECB = ecb.AsParallelWriter(),
                     Config = config,
                     ElapsedTime = elapsedTime,
                     Airports = _airports,
-                    PlanesSpawned = _planesSpawned
+                    CounterRef = counterRef
                 }.ScheduleParallel(state.Dependency);
                 state.Dependency.Complete();
-                configRW.ValueRW.CurrentPlaneCount += _planesSpawned.Value;
                 break;
         }
+
+        configRW.ValueRW.CurrentPlaneCount += *counterRef;
     }
 
     [BurstCompile]
@@ -137,16 +144,17 @@ public partial struct SpawnPlanesSingle : IJobEntity
     public ConfigComponent Config;
     public double ElapsedTime;
     [ReadOnly] public NativeArray<LocalTransform> Airports;
-    public NativeReference<int> PlanesSpawned;
+    [NativeDisableUnsafePtrRestriction] public unsafe int* CounterRef;
 
-    private void Execute(ref AirportComponent sourceComponent, in LocalTransform sourceTransform)
+    private unsafe void Execute(ref AirportComponent sourceComponent, in LocalTransform sourceTransform)
     {
         if (ElapsedTime < sourceComponent.NextPlaneSpawnTime) return;
-        if (Config.CurrentPlaneCount + PlanesSpawned.Value >= Config.MaxPlaneCount) return;
-        PlanesSpawned.Value++;
+        if (Config.CurrentPlaneCount + *CounterRef >= Config.MaxPlaneCount) return;
+        Interlocked.Increment(ref UnsafeUtility.AsRef<int>(CounterRef));
 
         var random = new Random((uint)ElapsedTime + 100);
-        sourceComponent.NextPlaneSpawnTime += random.NextDouble(Config.NextPlaneSpawnTimeLower, Config.NextPlaneSpawnTimeUpper);
+        sourceComponent.NextPlaneSpawnTime +=
+            random.NextDouble(Config.NextPlaneSpawnTimeLower, Config.NextPlaneSpawnTimeUpper);
         var planeAndGuideEntity = ECB.Instantiate(Config.PlanePrefab);
 
         var di = math.abs(random.NextInt()) % Airports.Length;
@@ -192,16 +200,18 @@ public partial struct SpawnPlanesParallel : IJobEntity
     public ConfigComponent Config;
     public double ElapsedTime;
     [ReadOnly] public NativeArray<LocalTransform> Airports;
-    [NativeDisableParallelForRestriction] public NativeReference<int> PlanesSpawned;
+    [NativeDisableUnsafePtrRestriction] public unsafe int* CounterRef;
 
-    private void Execute([ChunkIndexInQuery] int chunkIndex, ref AirportComponent sourceComponent, in LocalTransform sourceTransform)
+    private unsafe void Execute([ChunkIndexInQuery] int chunkIndex, ref AirportComponent sourceComponent,
+        in LocalTransform sourceTransform)
     {
         if (ElapsedTime < sourceComponent.NextPlaneSpawnTime) return;
-        if (Config.CurrentPlaneCount + PlanesSpawned.Value >= Config.MaxPlaneCount) return;
-        PlanesSpawned.Value++;
+        if (Config.CurrentPlaneCount + *CounterRef >= Config.MaxPlaneCount) return;
+        Interlocked.Increment(ref UnsafeUtility.AsRef<int>(CounterRef));
 
         var random = new Random((uint)ElapsedTime + 100);
-        sourceComponent.NextPlaneSpawnTime += random.NextDouble(Config.NextPlaneSpawnTimeLower, Config.NextPlaneSpawnTimeUpper);
+        sourceComponent.NextPlaneSpawnTime +=
+            random.NextDouble(Config.NextPlaneSpawnTimeLower, Config.NextPlaneSpawnTimeUpper);
         var planeAndGuideEntity = ECB.Instantiate(chunkIndex, Config.PlanePrefab);
 
         var di = math.abs(random.NextInt()) % Airports.Length;
